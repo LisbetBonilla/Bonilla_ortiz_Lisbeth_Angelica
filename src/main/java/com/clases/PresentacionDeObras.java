2b50e9b9e9cc5869d0149bf8e971dcf337a95254
LisbethBonilla <bonillaortiz.lisbeth@tiernoparla.es>
2023-02-22 23:58:42 +0100

    se crea clase resentacionDeObras

--- src/main/java/com/clases/PresentacionDeObras.java
+++ src/main/java/com/clases/PresentacionDeObras.java
@@ -1,5 +1,22 @@
 package main.java.com.clases;
 
 public class PresentacionDeObras {
-    
+
+    public static void main(String[] args) {
+
+        ObraDeArte obra1 = new ObraDeArte(001, "Guernica", "P.Picasso", "Óleo", null, 1000, 5, 2, 5,
+                "cuadro guerra civil");
+        ObraDeArte obra2 = new ObraDeArte(002, "La Vie", "P.Picasso", "Óleo", null, 200, 1, 1, 1, "óleo");
+        ObraDeArte obra3 = new ObraDeArte(003, "El Sueño", "P.Picasso", "Óleo", null, 300, 1.3, 1, 1, "óleo");
+        ObraDeArte obra4 = new ObraDeArte(004, "Retrato de Dora Maar", "P.Picasso", "Óleo", null, 400, 0.8, 1, 5,
+                "óleo");
+        ObraDeArte obra5 = new ObraDeArte(005, "El piel roja", "U.Checa", null, "Escultura", 350, 0.8, 2, 1,
+                "escultura");
+
+        ObraDeArte[] todasLasObras = { obra1, obra2, obra3, obra4, obra5 };
+
+        for (int n = 0; n < todasLasObras.length; n++) {
+            System.out.println(todasLasObras[n]);
+        }
+    }
 }
