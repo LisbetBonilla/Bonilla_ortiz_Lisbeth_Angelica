3d1979f1876e05fbcac08843d66538805393c18e
LisbethBonilla <bonillaortiz.lisbeth@tiernoparla.es>
2023-02-23 01:01:00 +0100

    modifico datos de la clase obra de arte

--- src/main/java/com/clases/ObraDeArte.java
+++ src/main/java/com/clases/ObraDeArte.java
@@ -30,28 +30,6 @@ public class ObraDeArte {
         this.descripcion = descripcion;
     }
 
-    public void verObras(String nombre) {
-        System.out.println(ArrayObras);
-    }
-
-    public static void vizaulizarDatos(String[] args) {
-
-        ObraDeArte obra1 = new ObraDeArte(001, "Guernica", "P.Picasso", "Óleo", null, 1000, 5, 2, 5,
-                "cuadro guerra civil");
-        ObraDeArte obra2 = new ObraDeArte(002, "La Vie", "P.Picasso", "Óleo", null, 200, 1, 1, 1, "óleo");
-        ObraDeArte obra3 = new ObraDeArte(003, "El Sueño", "P.Picasso", "Óleo", null, 300, 1.3, 1, 1, "óleo");
-        ObraDeArte obra4 = new ObraDeArte(004, "Retrato de Dora Maar", "P.Picasso", "Óleo", null, 400, 0.8, 1, 5,
-                "óleo");
-        ObraDeArte obra5 = new ObraDeArte(005, "El piel roja", "U.Checa", null, "Escultura", 350, 0.8, 2, 1,
-                "escultura");
-
-        ObraDeArte[] ArrayDeObras = { obra1, obra2, obra3, obra4, obra5 };
-
-        for (int n = 0; n < ArrayDeObras.length; n++) {
-            System.out.println(ArrayDeObras[n]);
-        }
-    }
-
     /* GETTERS************************************ */
     public int getId() {
         return id;
